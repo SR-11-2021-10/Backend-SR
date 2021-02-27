f6c6a1781ecd7dbb3e1be7292230ffa678c2b63c
Geovanny Gonzalez-Rodriguez <ga.gonzalezr@uniandes.edu.co>
2021-02-27 18:16:31 -0500

    Warning fixed

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_url():
     Get server's host and port
     """
     host = os.getenv("HOST", "0.0.0.0")
-    port = os.getenv("PORT", "8000")
+    port = os.getenv("PORT", 8000)
     return host, port
 
 
