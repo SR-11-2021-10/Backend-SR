1b7b30ff1b09a13bf503b63df91929056ab61fcd
Juan Manuel Saenz <saenzjm@gmail.com>
2021-03-17 08:17:08 -0500

    Mapero ORM de la base de datos

--- db/models.py
+++ db/models.py
@@ -5,7 +5,8 @@
     SQLAlchemy handles this in a simple and easy way. Please read the next reference
     https://fastapi.tiangolo.com/tutorial/sql-databases/?h=sql#create-the-database-models
 """
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, Float
+from sqlalchemy.orm import relationship
 from .database import Base
 
 # Base is the base model we instanciate in database.py module
@@ -19,8 +20,54 @@ class User(Base):
     """
 
     # Table name in DB
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     # Table Fields
-    username = Column(String, primary_key=True, index=True)
+    user_id = Column(String(250), primary_key=True, index=True)
     hashed_password = Column(String)
+    gender = Column(String(2))
+    age = Column(Integer)
+    country = Column(String(250))
+#    username = Column(String, primary_key=True, index=True)
+#    hashed_password = Column(String)
+
+
+class Artist(Base):
+    """
+    Artist model. This represent the basic artist info
+    """
+
+    # Table name in DB
+    __tablename__ = "artist"
+
+    # Table Fields
+    artist_id = Column(String(250), primary_key=True, index=True)
+    artist_name = Column(String(250), nullable=False)
+
+
+class Rating(Base):
+    """
+    Artist model. This represent the basic artist info
+    """
+
+    # Table name in DB
+    __tablename__ = "rating"
+
+    # Table Fields    
+    usert_id = relationship("User", back_populates="user_id")
+    artist_id = relationship("Artist", back_populates="artist_id")
+    rating = Column(Integer, nullable=False)
+
+
+class Estimation(Base):
+    """
+    Artist model. This represent the basic artist info
+    """
+
+    # Table name in DB
+    __tablename__ = "estimation"
+
+    # Table Fields    
+    usert_id = relationship("User", back_populates="user_id")
+    artist_id = relationship("Artist", back_populates="artist_id")
+    estimation = Column(Float, nullable=False)
